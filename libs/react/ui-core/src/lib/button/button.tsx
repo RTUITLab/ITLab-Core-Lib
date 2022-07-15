a7f2d4da68eba50b1a8ca430d76a43ee9ffa253c
Kirill <kirill.baikin@gmail.com>
2022-07-15 21:23:22 +0300

    Added tests

--- libs/react/ui-core/src/lib/button/button.tsx
+++ libs/react/ui-core/src/lib/button/button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps {}
 export function Button(props: ButtonProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
+      <h1>Welcome to Button (test)!</h1>
     </div>
   );
 }
