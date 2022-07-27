a17fe982ee5e13d4642431d3cecb66a155972318
Zayac11 <brunogrbstudios@gmail.com>
2022-07-27 17:23:52 +0300

    added test

--- libs/react/ui-core/src/lib/input/input.spec.tsx
+++ libs/react/ui-core/src/lib/input/input.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import {getByRole, render} from '@testing-library/react'
 
 import Input from './input';
 
@@ -6,5 +6,12 @@ describe('Input', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Input />);
     expect(baseElement).toBeTruthy();
+    expect(getByRole(baseElement, 'textbox')).toHaveProperty('value', '')
+  });
+
+  it('should have current value', () => {
+    const { baseElement } = render(<Input name={'myName'} defaultValue={'default value 3'} />);
+    expect(getByRole(baseElement, 'textbox')).toHaveProperty('name', 'myName')
+    expect(getByRole(baseElement, 'textbox')).toHaveProperty('value', 'default value 3')
   });
 });
