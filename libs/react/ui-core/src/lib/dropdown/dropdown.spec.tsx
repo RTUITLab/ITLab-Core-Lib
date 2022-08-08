06779959d969e2c5ed570aec34b358f6c5a0bfca
Zayac11 <brunogrbstudios@gmail.com>
2022-08-08 14:27:04 +0300

    dropdown test fix

--- libs/react/ui-core/src/lib/dropdown/dropdown.spec.tsx
+++ libs/react/ui-core/src/lib/dropdown/dropdown.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import Dropdown from './dropdown';
+import {Dropdown} from './dropdown';
 
 describe('Dropdown', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Dropdown />);
+    const { baseElement } = render(<Dropdown items={[{label: 'label', key: 'key1'}]} />);
     expect(baseElement).toBeTruthy();
   });
 });
