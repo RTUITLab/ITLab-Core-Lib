dfd0a1fd0e0d2003eff4e80fc9714cd0530bd233
temchik00 <temchik0020@gmail.com>
2022-08-24 22:01:33 +0300

    Fixed tests

--- libs/react/ui-core/src/lib/collapse/collapse.spec.tsx
+++ libs/react/ui-core/src/lib/collapse/collapse.spec.tsx
@@ -1,10 +1,9 @@
 import { render } from '@testing-library/react';
-
-import Collapse from './collapse';
+import { Collapse } from './collapse';
 
 describe('Collapse', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Collapse />);
+    const { baseElement } = render(<Collapse items={[]} />);
     expect(baseElement).toBeTruthy();
   });
 });
